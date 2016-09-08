ba6942a7bacbe2098798c0b83bc9edeb36f3c2de
Nate Irwin <nate@irwin.xyz>
2016-09-08 10:26:33 -0600

    Backup script

---
+++ backup-carto/index.js
@@ -0,0 +1,76 @@
+var configs = require('./configs.json');
+var directory = './_backups';
+var done = 0;
+var fs = require('fs');
+var queries = [];
+var request = require('request');
+var timestamp = new Date().getTime();
+var count = 0;
+var interval;
+
+if (!fs.existsSync(directory)) {
+  fs.mkdirSync(directory);
+}
+
+for (var userName in configs) {
+  var config = configs[userName];
+  var apiKey = config.api_key;
+
+  if (!fs.existsSync(directory + '/' + userName)) {
+    fs.mkdirSync(directory + '/' + userName);
+  }
+
+  fs.mkdirSync(directory + '/' + userName + '/' + timestamp);
+
+  config.tables.forEach(function (table) {
+    queries.push({
+      apiKey: apiKey,
+      table: table,
+      userName: userName
+    });
+  });
+}
+
+count = queries.length * 2;
+
+queries.forEach(function (query) {
+  request('https://' + query.userName + '.carto.com/api/v2/sql?api_key=' + query.apiKey + '&format=csv&q=SELECT * FROM ' + query.table, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      fs.writeFile(directory + '/' + query.userName + '/' + timestamp + '/' + query.table + '.csv', body, function (error) {
+        done++;
+
+        if (error) {
+          console.log('Error writing (CSV): ' + query.userName + '/' + query.table);
+        } else {
+          console.log('Success: ' + done + ' of ' + count);
+        }
+      });
+    } else {
+      done++;
+      console.log('Error ' + response.statusCode + ' downloading: https://' + query.userName + '.carto.com/api/v2/sql?api_key=' + query.apiKey + '&format=csv&q=SELECT * FROM ' + query.table);
+    }
+  });
+  request('https://' + query.userName + '.carto.com/api/v2/sql?api_key=' + query.apiKey + '&format=geojson&q=SELECT * FROM ' + query.table, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      fs.writeFile(directory + '/' + query.userName + '/' + timestamp + '/' + query.table + '.geojson', body, function (error) {
+        done++;
+
+        if (error) {
+          console.log('Error writing (GeoJSON): ' + query.userName + '/' + query.table);
+        } else {
+          console.log('Success: ' + done + ' of ' + count);
+        }
+      });
+    } else {
+      done++;
+      console.log('Error ' + response.statusCode + ' downloading (GeoJSON): https://' + query.userName + '.carto.com/api/v2/sql?api_key=' + query.apiKey + '&format=geojson&q=SELECT * FROM ' + query.table);
+    }
+  });
+});
+
+interval = setInterval(function () {
+  if (count === done) {
+    clearInterval(interval);
+    console.log('Done!');
+  }
+}, 500);
